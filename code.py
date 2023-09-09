c5a6c8b6f23471e420bb33790809462294baf271
GhalbeYou <arashaminiasdf@gmail.com>
2023-09-09 20:37:08 +0330

    Added kalameh font

---
+++ code.py
@@ -0,0 +1,21 @@
+text = """
+fonts
+typography
+Persian fonts
+creative projects
+graphic design
+web design
+font repository
+font collection
+open source
+contribution
+fontiran
+legal fonts
+free fonts
+typography resources
+font exploration
+"""
+
+# detect enters and replace them with `,`.
+
+print(text)
